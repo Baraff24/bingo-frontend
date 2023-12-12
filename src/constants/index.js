97896e6e72bad8cff7e195652be019f1c7276a75
Raffaele Grieco <62102593+Baraff24@users.noreply.github.com>
2023-12-12 17:10:30 +0100

    Minor Update

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@
 
 // export const { BASE_URL } = "http://localhost:8000";
 
-export const API_URL = "https://tombolata-backend.aup.it/api/v1";
+export const API_URL = "https://tombola-backend.aup.it/api/v1";
 
 export const API_APP_URL = API_URL + "/bingo";
 
